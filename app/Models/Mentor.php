c773d4778b72a0b72fd1712885124f918998e61c
DynnoOttu <dynnoottu968@gmail.com>
2024-01-29 22:39:06 +0800

    [Mentor] - API Update

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -9,12 +9,12 @@ class Mentor extends Model
 {
     protected $table = 'mentors';
 
-    protected $cats = [
+    protected $casts = [
         'created_at' => 'datetime:Y-m-d H:m:s',
-        'update' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s'
     ];
 
     protected $fillable = [
-        'name', 'profile', 'email', 'profession',
+        'name', 'profile', 'email', 'profession'
     ];
 }

1834587a38dd7981a40ab93df8776ae6c52ee131
DynnoOttu <dynnoottu968@gmail.com>
2024-05-05 21:47:47 +0800

    API Create Premium Access

--- app/Http/Controllers/LessonCntroller.php
+++ app/Http/Controllers/LessonCntroller.php
@@ -50,24 +50,23 @@ class LessonCntroller extends Controller
             'chapter_id' => 'required|integer'
         ];
 
-        $data = request()->all();
+        $data = $request->all();
 
         $validator = Validator::make($data, $rules);
         if ($validator->fails()) {
             return response()->json([
                 'status' => 'error',
-                'message' => $validator->errors(),
+                'message' => $validator->errors()
             ], 400);
         }
 
         $chapterId = $request->input('chapter_id');
         $chapter = Chapter::find($chapterId);
-
         if (!$chapter) {
             return response()->json([
                 'status' => 'error',
                 'message' => 'chapter not found'
-            ], 404);
+            ]);
         }
 
         $lesson = Lesson::create($data);
